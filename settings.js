0ffe3b0d46d1b73ecf875e8d6773869ebd1302cc
Afryy <128119373+Afryy@users.noreply.github.com>
2023-03-22 10:29:31 +0700

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,10 +1,10 @@
 const fs = require('fs')
 
-global.creator = 'Zeeoneofc'// yourname
-global.MONGO_DB_URI = "" //database mongodb 
+global.creator = 'Afryy'// yourname
+global.MONGO_DB_URI = "mongodb+srv://dhimazsav:daplon123@cluster0.1zkgthq.mongodb.net/?retryWrites=true&w=majority" //database mongodb 
 global.ACTIVATION_TOKEN_SECRET = "-@Pqnap+@(/1jAPPnew/@10" //isi apa aja bebas
-global.your_email = "" //email
-global.email_password = "" //application password email
+global.your_email = "dhimazsav@gmail.com" //email
+global.email_password = "ezlrtdqsrfiernuh" //application password email
 global.limitCount = 10000
 global.YUOR_PORT = 8000
 global.loghandler = {
